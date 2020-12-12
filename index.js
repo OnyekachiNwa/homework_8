dfc17f027d140d93d9ea812d1a065a1712d1adaa
OnyekachiNwa <OnyekachiNwa@users.noreply.github.com>
2020-12-11 21:48:08 -0800

    complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//typewriter effect by codepen.io >> this portion of the code has not been modified
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -48,11 +49,10 @@ window.onload = function() {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
+
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
-
 //source: https://codepen.io/gschier/pen/jkivt
